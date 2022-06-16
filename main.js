54d2843945fd8e879d2224a8476a184f5791671e
Suryash756 <86527278+Suryash756@users.noreply.github.com>
2022-06-16 04:47:54 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,549 @@
+
+function go(){
+    search= document.getElementById("input_search").value;
+    a_href= document.getElementById("search");
+    if (search.toLowerCase()=="google") {
+        location.href="#section1";
+    } 
+    if (search.toLowerCase()=="amazon") {
+        location.href="#section1";
+    } 
+    if (search.toLowerCase()=="flipkart") {
+        location.href="#section1";
+    } 
+    if (search.toLowerCase()=="facebook") {
+        location.href="#section1";
+    } 
+    if (search.toLowerCase()=="youtube") {
+        location.href="#section2";
+    } 
+     if (search.toLowerCase()=="instagram") {
+        location.href="#section2";
+    } 
+    if (search.toLowerCase()=="twitter") {
+        location.href="#section2";
+    } 
+    if (search.toLowerCase()=="uber") {
+        location.href="#section2";
+    } 
+    if (search.toLowerCase()=="oyo") {
+        location.href="#section3";
+    } 
+    if (search.toLowerCase()=="ola") {
+        location.href="#section3";
+    } 
+    if (search.toLowerCase()=="zomato") {
+        location.href="#section3";
+    } 
+    if (search.toLowerCase()=="swiggy") {
+        location.href="#section3";
+    } 
+    if (search.toLowerCase()=="byjus") {
+        location.href="#section4";
+    }  
+    if (search.toLowerCase()=="vedantu") {
+        location.href="#section4";
+    } 
+    if (search.toLowerCase()=="whitehatjr") {
+        location.href="#section4";
+    } 
+    if (search.toLowerCase()=="whitehat jr") {
+        location.href="#section4";
+    } 
+    if (search.toLowerCase()=="whitehat") {
+        location.href="#section4";
+    } 
+    if (search.toLowerCase()=="akash") {
+        location.href="#section4";
+    } 
+    if (search.toLowerCase()=="wikipedia") {
+        location.href="#section5";
+    } 
+    if (search.toLowerCase()=="yahoo") {
+        location.href="#section5";
+    } 
+    if (search.toLowerCase()=="reddit") {
+        location.href="#section5";
+    }
+    if (search.toLowerCase()=="redit") {
+        location.href="#section5";
+    }  
+    if (search.toLowerCase()=="bing") {
+        location.href="#section5";
+    } 
+    if (search.toLowerCase()=="netflix") {
+        location.href="#section6";
+    }
+    if (search.toLowerCase()=="disney") {
+        location.href="#section6";
+    }
+    if (search.toLowerCase()=="disney+") {
+        location.href="#section6";
+    }
+    if (search.toLowerCase()=="disneyplus") {
+        location.href="#section6";
+    }
+    if (search.toLowerCase()=="disney plus") {
+        location.href="#section6";
+    }
+    if (search.toLowerCase()=="disneyplushotstar") {
+        location.href="#section6";
+    }
+    if (search.toLowerCase()=="disney plus hotstar") {
+        location.href="#section6";
+    }
+    if (search.toLowerCase()=="hotstar") {
+        location.href="#section6";
+    }
+    if (search.toLowerCase()=="amazonprime") {
+        location.href="#section6";
+    }
+    if (search.toLowerCase()=="amazon prime") {
+        location.href="#section6";
+    }
+    if (search.toLowerCase()=="prime") {
+        location.href="#section6";
+    }
+    if (search.toLowerCase()=="primevideo") {
+        location.href="#section6";
+    }
+    if (search.toLowerCase()=="prime video") {
+        location.href="#section6";
+    }
+    if (search.toLowerCase()=="ebay") {
+        location.href="#section6";
+    }
+    if (search.toLowerCase()=="walmart") {
+        location.href="#section7";
+    }
+    if (search.toLowerCase()=="wallmart") {
+        location.href="#section7";
+    }
+    if (search.toLowerCase()=="espn") {
+        location.href="#section7";
+    }
+    if (search.toLowerCase()=="fandom") {
+        location.href="#section7";
+    }
+    if (search.toLowerCase()=="cnn") {
+        location.href="#section7";
+    }
+    if (search.toLowerCase()=="foxnews") {
+        location.href="#section8";
+    }
+    if (search.toLowerCase()=="fox news") {
+        location.href="#section8";
+    }
+    if (search.toLowerCase()=="microsoft") {
+        location.href="#section8";
+    }
+    if (search.toLowerCase()=="twitch") {
+        location.href="#section8";
+    }
+    if (search.toLowerCase()=="craigslist") {
+        location.href="#section8";
+    }
+    if (search.toLowerCase()=="outlook") {
+        location.href="#section9";
+    }
+    if (search.toLowerCase()=="outlok") {
+        location.href="#section9";
+    }
+    if (search.toLowerCase()=="instructure") {
+        location.href="#section9";
+    }
+    if (search.toLowerCase()=="theweatherchannel") {
+        location.href="#section9";
+    }
+    if (search.toLowerCase()=="the weather channel") {
+        location.href="#section9";
+    }
+    if (search.toLowerCase()=="office") {
+        location.href="#section9";
+    }
+    if (search.toLowerCase()=="microsoftoffice") {
+        location.href="#section9";
+    }
+    if (search.toLowerCase()=="microsoft office") {
+        location.href="#section9";
+    }
+    if (search.toLowerCase()=="accuweather") {
+        location.href="#section10";
+    }
+    if (search.toLowerCase()=="accu weather") {
+        location.href="#section10";
+    }
+    if (search.toLowerCase()=="zillow") {
+        location.href="#section10";
+    }
+    if (search.toLowerCase()=="zilow") {
+        location.href="#section10";
+    }
+    if (search.toLowerCase()=="zillo") {
+        location.href="#section10";
+    }
+    if (search.toLowerCase()=="zilo") {
+        location.href="#section10";
+    }
+    if (search.toLowerCase()=="imdb") {
+        location.href="#section10";
+    }
+    if (search.toLowerCase()=="u.s.p.s") {
+        location.href="#section10";
+    }
+    if (search.toLowerCase()=="usps") {
+        location.href="#section10";
+    }
+    if (search.toLowerCase()=="unitedstatespostalservice") {
+        location.href="#section10";
+    }
+    if (search.toLowerCase()=="unitedstates postal service") {
+        location.href="#section10";
+    }
+    if (search.toLowerCase()=="united states postal service") {
+        location.href="#section10";
+    }
+    if (search.toLowerCase()=="pintrest") {
+        location.href="#section11";
+    }
+    if (search.toLowerCase()=="target") {
+        location.href="#section11";
+    }
+    if (search.toLowerCase()=="quizelt") {
+        location.href="#section11";
+    }
+    if (search.toLowerCase()=="linkedin") {
+        location.href="#section11";
+    }
+    if (search.toLowerCase()=="linked in") {
+        location.href="#section11";
+    }
+    if (search.toLowerCase()=="thenewyorktimes") {
+        location.href="#section12";
+    }
+    if (search.toLowerCase()=="the newyork times") {
+        location.href="#section12";
+    }
+    if (search.toLowerCase()=="nytimes") {
+        location.href="#section12";
+    }
+    if (search.toLowerCase()=="duckduckgo") {
+        location.href="#section12";
+    }
+    if (search.toLowerCase()=="duck duck go") {
+        location.href="#section12";
+    }
+    if (search.toLowerCase()=="msn") {
+        location.href="#section12";
+    }
+    if (search.toLowerCase()=="zoom") {
+        location.href="#section12";
+    }
+    if (search.toLowerCase()=="outbrain") {
+        location.href="#section13";
+    }
+    if (search.toLowerCase()=="out brain") {
+        location.href="#section13";
+    }
+    if (search.toLowerCase()=="healthline") {
+        location.href="#section13";
+    }
+    if (search.toLowerCase()=="xfinity") {
+        location.href="#section13";
+    }
+    if (search.toLowerCase()=="x finity") {
+        location.href="#section13";
+    }
+    if (search.toLowerCase()=="tata") {
+        location.href="#section14";
+    }
+    if(search.toLowerCase()=="reliance"){
+        location.href="#section13"
+    }
+    if(search.toLowerCase()=="relaince"){
+        location.href="#section13"
+    }
+    if (search.toLowerCase()=="google ") {
+        location.href="#section1";
+    } 
+    if (search.toLowerCase()=="amazon ") {
+        location.href="#section1";
+    } 
+    if (search.toLowerCase()=="flipkart ") {
+        location.href="#section1";
+    } 
+    if (search.toLowerCase()=="facebook ") {
+        location.href="#section1";
+    } 
+    if (search.toLowerCase()=="youtube ") {
+        location.href="#section2";
+    } 
+     if (search.toLowerCase()=="instagram ") {
+        location.href="#section2";
+    } 
+    if (search.toLowerCase()=="twitter ") {
+        location.href="#section2";
+    } 
+    if (search.toLowerCase()=="uber ") {
+        location.href="#section2";
+    } 
+    if (search.toLowerCase()=="oyo ") {
+        location.href="#section3";
+    } 
+    if (search.toLowerCase()=="ola ") {
+        location.href="#section3";
+    } 
+    if (search.toLowerCase()=="zomato ") {
+        location.href="#section3";
+    } 
+    if (search.toLowerCase()=="swiggy ") {
+        location.href="#section3";
+    } 
+    if (search.toLowerCase()=="byjus ") {
+        location.href="#section4";
+    }  
+    if (search.toLowerCase()=="vedantu ") {
+        location.href="#section4";
+    } 
+    if (search.toLowerCase()=="whitehatjr ") {
+        location.href="#section4";
+    } 
+    if (search.toLowerCase()=="whitehat jr ") {
+        location.href="#section4";
+    } 
+    if (search.toLowerCase()=="whitehat ") {
+        location.href="#section4";
+    } 
+    if (search.toLowerCase()=="akash ") {
+        location.href="#section4";
+    } 
+    if (search.toLowerCase()=="wikipedia ") {
+        location.href="#section5";
+    } 
+    if (search.toLowerCase()=="yahoo ") {
+        location.href="#section5";
+    } 
+    if (search.toLowerCase()=="reddit ") {
+        location.href="#section5";
+    }
+    if (search.toLowerCase()=="redit ") {
+        location.href="#section5";
+    }  
+    if (search.toLowerCase()=="bing ") {
+        location.href="#section5";
+    } 
+    if (search.toLowerCase()=="netflix ") {
+        location.href="#section6";
+    }
+    if (search.toLowerCase()=="disney ") {
+        location.href="#section6";
+    }
+    if (search.toLowerCase()=="disney+ ") {
+        location.href="#section6";
+    }
+    if (search.toLowerCase()=="disneyplus ") {
+        location.href="#section6";
+    }
+    if (search.toLowerCase()=="disney plus ") {
+        location.href="#section6";
+    }
+    if (search.toLowerCase()=="disneyplushotstar ") {
+        location.href="#section6";
+    }
+    if (search.toLowerCase()=="disney plus hotstar ") {
+        location.href="#section6";
+    }
+    if (search.toLowerCase()=="hotstar ") {
+        location.href="#section6";
+    }
+    if (search.toLowerCase()=="amazonprime ") {
+        location.href="#section6";
+    }
+    if (search.toLowerCase()=="amazon prime ") {
+        location.href="#section6";
+    }
+    if (search.toLowerCase()=="prime ") {
+        location.href="#section6";
+    }
+    if (search.toLowerCase()=="primevideo ") {
+        location.href="#section6";
+    }
+    if (search.toLowerCase()=="prime video ") {
+        location.href="#section6";
+    }
+    if (search.toLowerCase()=="ebay ") {
+        location.href="#section6";
+    }
+    if (search.toLowerCase()=="walmart ") {
+        location.href="#section7";
+    }
+    if (search.toLowerCase()=="wallmart ") {
+        location.href="#section7";
+    }
+    if (search.toLowerCase()=="espn ") {
+        location.href="#section7";
+    }
+    if (search.toLowerCase()=="fandom ") {
+        location.href="#section7"; 
+    }
+    if (search.toLowerCase()=="cnn ") {
+        location.href="#section7";
+    }
+    if (search.toLowerCase()=="foxnews ") {
+        location.href="#section8";
+    }
+    if (search.toLowerCase()=="fox news ") {
+        location.href="#section8";
+    }
+    if (search.toLowerCase()=="microsoft ") {
+        location.href="#section8";
+    }
+    if (search.toLowerCase()=="twitch ") {
+        location.href="#section8";
+    }
+    if (search.toLowerCase()=="craigslist ") {
+        location.href="#section8";
+    }
+    if (search.toLowerCase()=="outlook ") {
+        location.href="#section9";
+    }
+    if (search.toLowerCase()=="outlok ") {
+        location.href="#section9";
+    }
+    if (search.toLowerCase()=="instructure ") {
+        location.href="#section9";
+    }
+    if (search.toLowerCase()=="theweatherchannel ") {
+        location.href="#section9";
+    }
+    if (search.toLowerCase()=="the weather channel ") {
+        location.href="#section9";
+    }
+    if (search.toLowerCase()=="office ") {
+        location.href="#section9";
+    }
+    if (search.toLowerCase()=="microsoftoffice ") {
+        location.href="#section9";
+    }
+    if (search.toLowerCase()=="microsoft office ") {
+        location.href="#section9";
+    }
+    if (search.toLowerCase()=="accuweather ") {
+        location.href="#section10";
+    }
+    if (search.toLowerCase()=="accu weather ") {
+        location.href="#section10";
+    }
+    if (search.toLowerCase()=="zillow ") {
+        location.href="#section10";
+    }
+    if (search.toLowerCase()=="zilow ") {
+        location.href="#section10";
+    }
+    if (search.toLowerCase()=="zillo ") {
+        location.href="#section10";
+    }
+    if (search.toLowerCase()=="zilo ") {
+        location.href="#section10";
+    }
+    if (search.toLowerCase()=="imdb ") {
+        location.href="#section10";
+    }
+    if (search.toLowerCase()=="u.s.p.s ") {
+        location.href="#section10";
+    }
+    if (search.toLowerCase()=="usps ") {
+        location.href="#section10";
+    }
+    if (search.toLowerCase()=="unitedstatespostalservice ") {
+        location.href="#section10";
+    }
+    if (search.toLowerCase()=="unitedstates postal service ") {
+        location.href="#section10";
+    }
+    if (search.toLowerCase()=="united states postal service ") {
+        location.href="#section10";
+    }
+    if (search.toLowerCase()=="pintrest ") {
+        location.href="#section11";
+    }
+    if (search.toLowerCase()=="target ") {
+        location.href="#section11";
+    }
+    if (search.toLowerCase()=="quizelt ") {
+        location.href="#section11";
+    }
+    if (search.toLowerCase()=="linkedin ") {
+        location.href="#section11";
+    }
+    if (search.toLowerCase()=="linked in ") {
+        location.href="#section11";
+    }
+    if (search.toLowerCase()=="thenewyorktimes ") {
+        location.href="#section12";
+    }
+    if (search.toLowerCase()=="the newyork times ") {
+        location.href="#section12";
+    }
+    if (search.toLowerCase()=="nytimes ") {
+        location.href="#section12";
+    }
+    if (search.toLowerCase()=="duckduckgo ") {
+        location.href="#section12";
+    }
+    if (search.toLowerCase()=="duck duck go ") {
+        location.href="#section12";
+    }
+    if (search.toLowerCase()=="msn ") {
+        location.href="#section12";
+    }
+    if (search.toLowerCase()=="zoom ") {
+        location.href="#section12";
+    }
+    if (search.toLowerCase()=="outbrain ") {
+        location.href="#section13";
+    }
+    if (search.toLowerCase()=="out brain ") {
+        location.href="#section13";
+    }
+    if (search.toLowerCase()=="healthline ") {
+        location.href="#section13";
+    }
+    if (search.toLowerCase()=="xfinity ") {
+        location.href="#section13";
+    }
+    if (search.toLowerCase()=="x finity ") {
+        location.href="#section13";
+    }
+    if (search.toLowerCase()=="tata ") {
+        location.href="#section14";
+    }
+    if(search.toLowerCase()=="reliance "){
+        location.href="#section13"
+    }
+    if(search.toLowerCase()=="relaince "){
+        location.href="#section13"
+    }
+    if(search.toLowerCase()=="apple"){
+        location.href="#section14"
+    }
+    if(search.toLowerCase()=="apple "){
+        location.href="#section14"
+    }
+    if(search.toLowerCase()=="alphabet"){
+        location.href="#section14"
+    }
+    if(search.toLowerCase()=="alphabet "){
+        location.href="#section14"
+    }
+    if(search.toLowerCase()=="tesla"){
+        location.href="#section14"
+    }
+    if(search.toLowerCase()=="tesla "){
+        location.href="#section14"
+    }
+    
+}
+
+
+
